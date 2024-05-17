57f8cb32619e102abcfdb8ff0db16b26ca5ccdcb
Md Shojib Hossain <mdshojib922@gmail.com>
2024-05-17 22:51:34 +0600

    Code Optimized

--- index.js
+++ index.js
@@ -128,9 +128,7 @@ async function run() {
         admin = user?.role === 'admin'
       }
       res.send({ admin });
-    })
-
-
+    });
 
     /*Menu Food item */
     app.get('/menu', async (req, res) => {
@@ -335,7 +333,7 @@ async function run() {
 
     // await client.connect();
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
