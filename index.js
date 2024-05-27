6e9dda574c02537dbecd262b04a82f607b3c7ed8
Md Shojib Hossain <mdshojib922@gmail.com>
2024-05-27 18:17:43 +0600

    Inventory Updated

--- index.js
+++ index.js
@@ -626,7 +626,7 @@ async function run() {
 
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
